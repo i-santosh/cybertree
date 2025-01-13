9e5ba06d2934379396f9fd4ab6d18b135c22d647
RohanKashyap <roy282227@gmail.com>
2025-01-13 16:50:02 +0530

    get_oauth_toke

---
+++ mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+    $mail_host = "smtp.gmail.com";
+    $mail_port = "587";
+    $mail_sender_email = "Cybertreeindia001@gmail.com"; //sender
+    $mail_sender_password = "13131313SAB"; //sender
+    $mail_sender_name = "Website Form";
+?>
